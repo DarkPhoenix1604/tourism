b6f870d7c8c6a022d019bb28448aa8d786c4aeb6
Aaditya Pahariya <aadityapahariya@Aadityas-MacBook-Air.local>
2025-06-21 19:03:09 +0530

    still images

--- sierra/next.config.ts
+++ sierra/next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com","plus.unsplash.com"],
-  },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.unsplash.com",
+      },
+    ],
+  }  
 };
 
 export default nextConfig;
